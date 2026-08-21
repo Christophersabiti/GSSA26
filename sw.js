42081e1bd366bae0c3f78d6036d6610e3e19ad0b
Christophersabiti <26697691+Christophersabiti@users.noreply.github.com>
2026-08-21 14:57:44 +0300

    Add sessions PWA and LinkedIn connect directory

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME='gssa-2026-v1';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./PMI-Logo-Uganda.png','./pmi-simple-png.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
+});
